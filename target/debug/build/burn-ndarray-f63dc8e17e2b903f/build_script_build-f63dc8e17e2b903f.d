2229bed6de37d93601108ffa68d6559cf8813e8a
keo-codes <keocodes@gmail.com>
2026-03-02 03:37:17 +0200

    Add files via upload

---
+++ target/debug/build/burn-ndarray-f63dc8e17e2b903f/build_script_build-f63dc8e17e2b903f.d
@@ -0,0 +1,5 @@
+C:\Users\makhu\word-doc-qa\target\debug\build\burn-ndarray-f63dc8e17e2b903f\build_script_build-f63dc8e17e2b903f.d: C:\Users\makhu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\burn-ndarray-0.20.1\build.rs
+
+C:\Users\makhu\word-doc-qa\target\debug\build\burn-ndarray-f63dc8e17e2b903f\build_script_build-f63dc8e17e2b903f.exe: C:\Users\makhu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\burn-ndarray-0.20.1\build.rs
+
+C:\Users\makhu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\burn-ndarray-0.20.1\build.rs:
